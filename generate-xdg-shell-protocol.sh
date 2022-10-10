47f717c61a1c46576a58ee424af4b42763a52e98
Mateusz Niemojewski <mateusz@niemojewski.eu>
2022-10-10 22:31:14 +0200

    Some window on a screen. No input yet.

---
+++ generate-xdg-shell-protocol.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# TODO: add good practices
+
+wayland-scanner private-code \
+  < /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml \
+  > xdg-shell-protocol.c
+wayland-scanner client-header \
+  < /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml \
+  > xdg-shell-client-protocol.h
--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <wayland-client.h>
 #include <cstring>
 #include "shm_tools.h"
+#include "xdg-shell-client-protocol.h"
+
+/// --------- client state
 
 struct client_state {
     /* Globals */
@@ -15,8 +18,12 @@ struct client_state {
     struct wl_surface *wl_surface;
     struct xdg_surface *xdg_surface;
     struct xdg_toplevel *xdg_toplevel;
+    /* State */
+    float offset;
+    uint32_t last_frame;
 };
 
+/// --------- wl registry listener
 
 static void
 registry_handle_global(void *data, struct wl_registry *wl_registry,
@@ -34,8 +41,11 @@ registry_handle_global(void *data, struct wl_registry *wl_registry,
         state->wl_shm = static_cast<wl_shm *>(wl_registry_bind(
                 wl_registry, name, &wl_shm_interface, 1));
     }
+    if (strcmp(interface, xdg_wm_base_interface.name) == 0) {
+        state->xdg_wm_base = static_cast<xdg_wm_base *>(wl_registry_bind(
+                wl_registry, name, &xdg_wm_base_interface, 1));
+    }
 }
-
 
 static void
 registry_handle_global_remove(void *data, struct wl_registry *registry,
@@ -50,59 +60,189 @@ static const struct wl_registry_listener
         .global_remove = registry_handle_global_remove,
 };
 
+/// --------- wl buffer listener
+
+static void
+wl_buffer_release(void *data, struct wl_buffer *wl_buffer)
+{
+    /* Sent by the compositor when it's no longer using this buffer */
+    wl_buffer_destroy(wl_buffer);
+}
+
+static const struct wl_buffer_listener wl_buffer_listener = {
+        .release = wl_buffer_release,
+};
+
+/// --------- xdg surface listener
+
+static struct wl_buffer *
+draw_frame(struct client_state *state)
+{
+    const int width = 640, height = 480;
+    int stride = width * 4;
+    int size = stride * height;
+
+    int fd = allocate_shm_file(size);
+    if (fd == -1) {
+        return NULL;
+    }
+
+    uint32_t *data = static_cast<uint32_t *>(mmap(NULL, size,
+                                                  PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+    if (data == MAP_FAILED) {
+        close(fd);
+        return NULL;
+    }
+
+    struct wl_shm_pool *pool = wl_shm_create_pool(state->wl_shm, fd, size);
+    struct wl_buffer *buffer = wl_shm_pool_create_buffer(pool, 0,
+                                                         width, height, stride, WL_SHM_FORMAT_XRGB8888);
+    wl_shm_pool_destroy(pool);
+    close(fd);
+
+    /* Draw checkerboxed background */
+    int offset = (int)state->offset % 8;
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+//            if ((x + y / 8 * 8) % 16 < 8)
+        	if (((x + offset) + (y + offset) / 8 * 8) % 16 < 8)
+                    data[y * width + x] = 0xFF666666;
+            else
+                data[y * width + x] = 0xFFaaEEaa;
+        }
+    }
+
+    munmap(data, size);
+    wl_buffer_add_listener(buffer, &wl_buffer_listener, NULL);
+    return buffer;
+}
+
+static void
+xdg_surface_configure(void *data,
+                      struct xdg_surface *xdg_surface, uint32_t serial)
+{
+    struct client_state *state = static_cast<client_state *>(data);
+    xdg_surface_ack_configure(xdg_surface, serial);
+
+    struct wl_buffer *buffer = draw_frame(state);
+    wl_surface_attach(state->wl_surface, buffer, 0, 0);
+    wl_surface_commit(state->wl_surface);
+}
+
+static const struct xdg_surface_listener xdg_surface_listener = {
+        .configure = xdg_surface_configure,
+};
+
+/// --------- xdg surface frame listener
+
+extern /*static*/ const struct wl_callback_listener wl_surface_frame_listener;
+
+static void
+wl_surface_frame_done(void *data, struct wl_callback *cb, uint32_t time)
+{
+	/* Destroy this callback */
+	wl_callback_destroy(cb);
+
+	/* Request another frame */
+	struct client_state *state = static_cast<client_state *>(data);
+	cb = wl_surface_frame(state->wl_surface);
+	wl_callback_add_listener(cb, &wl_surface_frame_listener, state);
+
+	/* Update scroll amount at 24 pixels per second */
+	if (state->last_frame != 0) {
+		int elapsed = time - state->last_frame;
+		state->offset += elapsed / 1000.0 * 24;
+	}
+
+	/* Submit a frame for this event */
+	struct wl_buffer *buffer = draw_frame(state);
+	wl_surface_attach(state->wl_surface, buffer, 0, 0);
+//	wl_surface_damage_buffer(state->wl_surface, 0, 0, INT32_MAX, INT32_MAX);
+	wl_surface_damage_buffer(state->wl_surface, 0, 0, 20, INT32_MAX);
+	wl_surface_commit(state->wl_surface);
+
+	state->last_frame = time;
+}
+
+/*static*/ const struct wl_callback_listener wl_surface_frame_listener = {
+	.done = wl_surface_frame_done,
+};
+
+
+/// --------- main
+
 int
 main(int argc, char *argv[])
 {
     struct client_state state = {0 };
 
     /// connect display
-    struct wl_display *display = wl_display_connect(NULL);
-    struct wl_registry *registry = wl_display_get_registry(display);
-    wl_registry_add_listener(registry, &registry_listener, &state);
+    state.wl_display = wl_display_connect(NULL);
+    state.wl_registry = wl_display_get_registry(state.wl_display);
+    wl_registry_add_listener(state.wl_registry, &registry_listener, &state);
 //    wl_shm_add_listener(registry, &registry_listener, &state);
-    wl_display_roundtrip(display);
+    wl_display_roundtrip(state.wl_display);
 
     /// create surface
-    struct wl_surface *surface = wl_compositor_create_surface(state.wl_compositor);
-
-    /// create mem pool
-    const int width = 1920, height = 1080;
-    const int stride = width * 4;
-    const int shm_pool_size = height * stride * 2;
-
-    int fd = allocate_shm_file(shm_pool_size);
-    uint8_t *pool_data = static_cast<uint8_t *>(mmap(NULL, shm_pool_size,
-                                                     PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
-
-    struct wl_shm *shm = state.wl_shm; // Bound from registry
-    struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, shm_pool_size);
-
-    /// create buffer from pool
-    int index = 0;
-    int offset = height * stride * index;
-    struct wl_buffer *buffer = wl_shm_pool_create_buffer(pool, offset,
-                                                         width, height, stride, WL_SHM_FORMAT_XRGB8888);
-
-    /// set memdata to something
-    uint32_t *pixels = (uint32_t *)&pool_data[offset];
-    memset(pixels, 0, width * height * 4);
-
-    /// checker perhaps
+    state.wl_surface = wl_compositor_create_surface(state.wl_compositor);
+//
+//    /// create mem pool
+//    const int width = 1920, height = 1080;
+//    const int stride = width * 4;
+//    const int shm_pool_size = height * stride * 2;
+//
+//    int fd = allocate_shm_file(shm_pool_size);
+//    uint8_t *pool_data = static_cast<uint8_t *>(mmap(NULL, shm_pool_size,
+//                                                     PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+//
+//    struct wl_shm *shm = state.wl_shm; // Bound from registry
+//    struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, shm_pool_size);
+//
+//    /// create buffer from pool
+//    int index = 0;
+//    int offset = height * stride * index;
+//    struct wl_buffer *buffer = wl_shm_pool_create_buffer(pool, offset,
+//                                                         width, height, stride, WL_SHM_FORMAT_XRGB8888);
+//
+//    /// set memdata to something
 //    uint32_t *pixels = (uint32_t *)&pool_data[offset];
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            if ((x + y / 8 * 8) % 16 < 8) {
-                pixels[y * width + x] = 0xFF666666;
-            } else {
-                pixels[y * width + x] = 0xFFEEEEEE;
-            }
-        }
-    }
+//    memset(pixels, 0, width * height * 4);
+//
+//    /// checker perhaps
+////    uint32_t *pixels = (uint32_t *)&pool_data[offset];
+//    for (int y = 0; y < height; ++y) {
+//        for (int x = 0; x < width; ++x) {
+//            if ((x + y / 8 * 8) % 16 < 8) {
+//                pixels[y * width + x] = 0xFF666666;
+//            } else {
+//                pixels[y * width + x] = 0xFFEEEEEE;
+//            }
+//        }
+//    }
+//
+//    /// attach buffer to surface
+//    wl_surface_attach(state.wl_surface, buffer, 0, 0);
+//    wl_surface_damage(state.wl_surface, 0, 0, UINT32_MAX, UINT32_MAX);
+//    wl_surface_commit(state.wl_surface);
 
-    /// attach buffer to surface
-    wl_surface_attach(surface, buffer, 0, 0);
-    wl_surface_damage(surface, 0, 0, UINT32_MAX, UINT32_MAX);
-    wl_surface_commit(surface);
+    /// get xdg_surface
+    state.xdg_surface = xdg_wm_base_get_xdg_surface(
+            state.xdg_wm_base, state.wl_surface);
+
+    xdg_surface_add_listener(state.xdg_surface, &xdg_surface_listener, &state);
+
+    /// get toplevel
+    state.xdg_toplevel = xdg_surface_get_toplevel(state.xdg_surface);
+    xdg_toplevel_set_title(state.xdg_toplevel, "Example client");
+    wl_surface_commit(state.wl_surface);
+
+    struct wl_callback *cb = wl_surface_frame(state.wl_surface);
+    wl_callback_add_listener(cb, &wl_surface_frame_listener, &state);
+
+    /// dispatch loop
+    while (wl_display_dispatch(state.wl_display)) {
+        /* This space deliberately left blank */
+    }
 
     return 0;
 }
