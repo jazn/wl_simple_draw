225693e468c8d6ea1f08a9904640ff0eeaa7aabf
Mateusz Niemojewski <mateusz@niemojewski.eu>
2022-10-13 20:54:48 +0200

    Generate protocol files in cmake.

--- generate-xdg-shell-protocol.sh
+++ generate-xdg-shell-protocol.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # TODO: add good practices
-
+echo "Generate xdg-shell-protocol.c"
 wayland-scanner private-code \
   < /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml \
   > xdg-shell-protocol.c
+echo "Generate xdg-shell-protocol.h"
 wayland-scanner client-header \
   < /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml \
   > xdg-shell-client-protocol.h
